9c75c24b0264749374a59a6e63ee3ea878893fe5
Utkarsh <utkarshSrivastava07@outlook.com>
2023-08-21 14:08:14 +0530

    Basic funcationality for todo list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+  const addTask = () => {
+
+    const addedTask={
+      id:todoList.length ===0 ? 1: todoList[todoList.length-1].id+1,
+      name:newTask
+    }
+    const newTodoList = [...todoList, addedTask];
+    setTodoList(newTodoList);
+  };
+  const deleteTask = (id) => {
+    const newTodoList = todoList.filter((task) => {
+      if (task.id === id) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    setTodoList(newTodoList);
+  };
+
   return (
     <div className="App">
-       </div>
+      <div className="addTask">
+        <input onChange={handleChange} placeholder="Enter task Here"></input>
+        <button onClick={addTask}>+</button>
+      </div>
+      <div className="viewTsk">
+        {todoList.map((task) => {
+          return (
+            <div>
+              <h1>{task.name}</h1>
+              <button onClick={() => deleteTask(task.id)}>DELETE</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
